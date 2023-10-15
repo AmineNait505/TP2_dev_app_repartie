662730dec2cae9b371a04324e4b303e8cea93284
Nait Slimene Amine <137489572+AmineNait505@users.noreply.github.com>
2023-10-15 22:10:12 +0100

    commit

--- src/New/Operation.java
+++ src/New/Operation.java
@@ -1,43 +1,39 @@
 package New;
-
 import java.io.Serializable;
+//activite 2.2
+import java.net.ServerSocket;
+public class Operation implements Serializable {
+private int op1,op2,result;
+private char operation;// + ou - ou / ou %
+public Operation(int op1, int op2, char operation) {
+this.op1=op1;
+this.op2=op2;
+this.operation=operation;
+}
+public int getOp1() {
+return op1;
+}
+public void setOp1(int op1) {
+this.op1 = op1;
+}
+public int getOp2() {
+return op2;
+}
+public void setOp2(int op2) {
+this.op2 = op2;
+}
+public int getResult() {
+return result;
+}
+public void setResult(int result) {
+this.result = result;
+}
+public char getOperation() {
+return operation;
+}
+public void setOperation(char operation) {
+this.operation = operation;
+}
 
-
-public class Operation implements Serializable{
-	private int op1;
-	private int op2;
-	private int result;
-	private char operande;
-	public Operation(int op1, int op2, char operande) {
-		
-		this.op1 = op1;
-		this.op2 = op2;
-		this.operande = operande; //
-	}
-	public int getOp1() {
-		return op1;
-	}
-	public void setOp1(int op1) {
-		this.op1 = op1;
-	}
-	public int getOp2() {
-		return op2;
-	}
-	public void setOp2(int op2) {
-		this.op2 = op2;
-	}
-	public int getResult() {
-		return result;
-	}
-	public void setResult(int result) {
-		this.result = result;
-	}
-	public char getOperande() {
-		return operande;
-	}
-	public void setOperande(char operande) {
-		this.operande = operande;
-	}
-	
 
 }
